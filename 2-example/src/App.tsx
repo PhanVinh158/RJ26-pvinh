66e04fd012874d3512022ed7aca808cef37a8783
Vinh <vinhphan15082002@gmail.com>
2022-11-01 13:03:05 +0700

    RJ26-pvinh

---
+++ 2-example/src/App.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { type } from "os";
+import { NONAME } from "dns";
+
+
+interface IUser {
+
+}
+function App() {
+
+  return (
+    <div>
+      <div className="row">
+        <div className="col-6">
+            <p>aa</p>
+        </div>
+        <div className="col-6">
+            <p>aâ</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
